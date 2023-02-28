ab21f67a6bfd4ca4ea55b85037483a129a529250
jcarlosvcx <l19920191@voaxaca.tecnm.mx>
2023-02-28 13:57:26 -0600

    Initial commit

---
+++ backend-develop/src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+import factorymethod.IDBAdapter
+import factorymethod.MySqlDBAdapter
+import factorymethod.OracleDBAdapter
+import properties.PropertiesUtil
+import java.util.*
+
+fun main() {
+
+    //val property: Properties? = PropertiesUtil
+    //  .loadProperty("properties/DBMySQL")
+    //println(property)
+    val mySql: IDBAdapter = MySqlDBAdapter()
+    println(mySql.getConnection().toString())
+
+    val oracle: IDBAdapter = OracleDBAdapter()
+    println(oracle.getConnection().toString())
+
+    val  postgres : IDBAdapter = PostgreSqlDBAdapter()
+    println(postgres.getConnection().toString())
+
+
+
+}
+
+
+
+
+
+
