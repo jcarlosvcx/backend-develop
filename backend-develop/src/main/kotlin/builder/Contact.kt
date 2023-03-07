59b6adedb317bd6ef660be359c9ceef08544f021
jcarlosvcx <l19920191@voaxaca.tecnm.mx>
2023-03-07 12:55:00 -0600

    create builder method

---
+++ backend-develop/src/main/kotlin/builder/Contact.kt
@@ -0,0 +1,7 @@
+package builder
+
+data class Contact(val name: String?,
+                   val phone: Phone?,
+                   val address: Address?
+
+)
---
+++ backend-develop/src/main/kotlin/builder/Phone.kt
@@ -0,0 +1,7 @@
+package builder
+
+data class Phone (
+    val phoneNumber: String?,
+    val ext: String?,
+    val phoneType: String?)
+
